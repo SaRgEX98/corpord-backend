5011721e2d49eb693d46e14d9643dca3f5e701b7
SaRgEX <maze_98@mail.ru>
2025-11-04 19:26:27 +0500

    Added driver, driver status models

---
+++ model/driver.go
@@ -0,0 +1,33 @@
+package model
+
+import "errors"
+
+type Driver struct {
+	ID          int          `json:"id" db:"id"`
+	FirstName   string       `json:"first_name" db:"first_name"`
+	LastName    string       `json:"last_name" db:"last_name"`
+	MiddleName  string       `json:"middle_name" db:"middle_name"`
+	PhoneNumber string       `json:"phone_number" db:"phone_number"`
+	Status      DriverStatus `json:"status" db:"status"`
+}
+
+type DriverStatus struct {
+	ID   int    `json:"id" db:"id"`
+	Name string `jsong:"name" db:"name"`
+}
+
+type DriverOutput struct {
+	ID          int    `json:"-,omitempty" db:"id"`
+	FirstName   string `json:"first_name" db:"first_name"`
+	LastName    string `json:"last_name" db:"last_name"`
+	MiddleName  string `json:"middle_name" db:"middle_name"`
+	PhoneNumber string `json:"phone_number" db:"phone_number"`
+	Status      string `json:"status" db:"driver_status"`
+}
+
+func (ds *DriverStatus) Validate() error {
+	if &ds.ID == nil && &ds.Name == nil {
+		return errors.New("id or name is required")
+	}
+	return nil
+}
